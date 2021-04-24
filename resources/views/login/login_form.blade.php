2da72f39e6001ad04ea3fd013eacc7a853ee2723
tomoatsu <tomoatsu213@gmail.com>
2021-04-25 08:46:45 +0900

    ログインフォーム作成、バリデーション設定

---
+++ resources/views/login/login_form.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ログインフォーム</title>
+    <script src="{{ asset('js/app.js') }}" defer>
+    </script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/signin.css') }}">
+</head>
+
+
+<body class="text-center">
+    <main class="form-signin">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <h1 class="h3 mb-3 fw-normal">ログインフォーム</h1>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required
+                autofocus>
+            <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password"
+                required>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">ログイン</button>
+        </form>
+    </main>
+</body>
+
+</html>
